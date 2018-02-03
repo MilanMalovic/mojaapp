447454a2335037e895780bfe8b6cf5da0a256b16
Milan <milan.malovic@yahoo.com>
2018-02-03 19:24:27 +0100

    1.2

--- app/src/main/java/com/example/restaraunt/FirstActivity.java
+++ app/src/main/java/com/example/restaraunt/FirstActivity.java
@@ -1,9 +1,19 @@
 package com.example.restaraunt;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import com.example.restaraunt.provider.CategoryProvider;
+import com.example.restaraunt.provider.MealProvider;
+
+import java.util.List;
 
 public class FirstActivity extends Activity {
 
@@ -16,7 +26,23 @@ public class FirstActivity extends Activity {
         Toast toast = Toast.makeText(getBaseContext(), "FirstActivity.onCreate()", Toast.LENGTH_SHORT);
         toast.show();
 
+        final List<String> categoryNames = CategoryProvider.getCategoryNames();
 
+        // Creates an ArrayAdaptar from the array of String
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, R.layout.list_item, categoryNames);
+        ListView listView = (ListView) findViewById(R.id.listofMeals);
+
+        // Assigns ArrayAdaptar to ListView
+        listView.setAdapter(dataAdapter);
+
+        // Starts the SecondActivity and sends it the selected URL as an extra data
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                intent.putExtra("position", position);
+                startActivity(intent);
+            }
+        });
 
 
     }
